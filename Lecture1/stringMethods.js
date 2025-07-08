ebb7ac4bdb950c1db3a3005ebe69b266779bf616
ManSingh6795 <mansingh31nitkkr@gmail.com>
2025-07-08 07:39:40 +0000

    String methods

---
+++ Lecture1/stringMethods.js
@@ -0,0 +1,9 @@
+const userName = "Man Singh Yadav";
+console.log(userName.toUpperCase());
+console.log(userName.indexOf("S"));
+console.log(userName.charAt(2));
+console.log(userName.slice(0,3));
+console.log(userName.split(" ",1));
+console.log(userName.length);
+console.log(userName.replace(" ","-"));
+console.log(userName.trim());
