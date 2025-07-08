452d40dc8038dbd72ddd6aa162afc7d2fb66778b
ManSingh6795 <mansingh31nitkkr@gmail.com>
2025-07-08 08:16:31 +0000

    Number and Manth Methods

---
+++ Lecture1/number_Math.js
@@ -0,0 +1,12 @@
+let testNumber = 10.000;
+//console.log(testNumber.toFixed(2))
+// console.log(testNumber.toLocaleString())
+// console.log(testNumber.toPrecision(3))
+// console.log(testNumber.toString().length)
+
+//++++++++++++ Math +++++++++++++++++++++++
+
+console.log(Math.random());
+console.log(Math.round(testNumber))
+console.log(Math.SQRT2);
+console.log(Math.abs(testNumber))
