f68ddc293b2890acdd3afa76d90d9cf0f25739d5
ManSingh6795 <mansingh31nitkkr@gmail.com>
2025-07-08 11:40:39 +0000

    Date time methods

---
+++ Lecture1/dateTime.js
@@ -0,0 +1,8 @@
+let createDate = new Date();
+
+// console.log(createDate);
+// console.log(createDate.toDateString());
+// console.log(createDate.toLocaleDateString());
+// console.log(createDate.getDate());
+// console.log(createDate.getDate());
+console.log(createDate.getFullYear());
