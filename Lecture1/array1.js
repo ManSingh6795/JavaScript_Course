31ed657d98e076bfbf307905b9fc4e0e294f1488
ManSingh6795 <mansingh31nitkkr@gmail.com>
2025-07-09 04:15:43 +0000

    array first

---
+++ Lecture1/array1.js
@@ -0,0 +1,8 @@
+let createArray = [1,2,3,4,5];
+console.log(createArray);
+// console.log(createArray.length);
+// console.log(createArray.join());
+//console.log(createArray.unshift());
+//console.log(createArray.push(6));
+console.log(createArray.pop());
+console.log(createArray);
